d09d0bf4c3412263fc3d28bb2769c8a631254d50
zhurof <41473707+zhurof@users.noreply.github.com>
2019-07-04 10:38:37 +0200

    Доработки после релиза
    Добавил виджет для покупки и состояния кнопок

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 ﻿$('.top-block').slick({
 	arrows: false,
+	autoplay: true,
+	autoplaySpeed: 5000,
+	pauseOnHover: false,
 	responsive: [
 		{
 			breakpoint: 1140,
@@ -8,4 +11,8 @@
 			}
 		}
 	]
+})
+$('.widget__head-btn').click(function(e){
+	e.preventDefault();
+	$(this).parent('.widget').toggleClass('active');
 })
